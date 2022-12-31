8affabfa618fba187bcf49c5d9c6c128a3614078
0x8008 <balu@xdapierdolnik.pl>
2022-12-31 11:33:14 +0100

    ssh fix

--- nixos/presets/laptop.nix
+++ nixos/presets/laptop.nix
@@ -10,11 +10,4 @@
       CPU_BOOST_ON_BAT = 0;
     };
   };
-
-  # Personal preference on how logind should handle lid switch.
-  services.logind = {
-    lidSwitch = "suspend";
-    lidSwitchDocked = "ignore";
-    lidSwitchExternalPower = "lock";
-  };
 }
