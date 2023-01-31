62bde3874401c14c750c82c1eca186163d47d0c1
0x8008 <balu@xdapierdolnik.pl>
2023-01-31 11:21:20 +0100

    +KDE module

---
+++ nixos/modules/de-kde.nix
@@ -0,0 +1,37 @@
+{ pkgs, ... }: {
+  services.xserver = {
+    enable = true;
+    displayManager.sddm = {
+      enable = true;
+    };
+    desktopManager.plasma5.enable = true;
+  };
+
+  # Debloat
+  services.xserver.desktopManager.plasma5.excludePackages = with pkgs.libsForQt5; [
+    khelpcenter
+    print-manager 
+    oxygen
+  ]
+
+  # Other software
+  environment.systemPackages = with pkgs; [
+    # Nothing necessary here so far.
+  ];
+
+  fonts = {
+    fonts = with pkgs; [
+      apple-emoji-linux
+      cascadia-code # S-tier font for terminal
+      noto-fonts
+      noto-fonts-cjk
+    ];
+    fontconfig.defaultFonts.emoji = [ "Apple Color Emoji" ];
+  };
+
+  programs.gamemode.enable = true;
+  security.unprivilegedUsernsClone = true;
+  services.flatpak.enable = true;
+  services.power-profiles-daemon.enable = false;
+  programs.dconf.enable = true;
+}
